4756360969ce2a4d5e3b34ef03e8cc1cd8141808
Sahana <132800002+sahanpatil28@users.noreply.github.com>
2024-07-31 13:24:07 +0530

    Updates

---
+++ precache-manifest.92c3ef167063bc033251ce14437d7422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b281756e5270e42cc18aac825c541cbb",
+    "url": "/FullstackManager/index.html"
+  },
+  {
+    "revision": "c926114eee5bfb2c52cd",
+    "url": "/FullstackManager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b94f113d008c37cb589e",
+    "url": "/FullstackManager/static/js/2.6470c3d0.chunk.js"
+  },
+  {
+    "revision": "a419c9f0ce456bb726d4",
+    "url": "/FullstackManager/static/js/3.66c6ba4f.chunk.js"
+  },
+  {
+    "revision": "c926114eee5bfb2c52cd",
+    "url": "/FullstackManager/static/js/main.acac0b9e.chunk.js"
+  },
+  {
+    "revision": "571851f91cbe1e07d076",
+    "url": "/FullstackManager/static/js/runtime~main.d2f269bc.js"
+  }
+]);
